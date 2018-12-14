7eb45898a18a5a6957940acb511b22f1d7d5c631
Gabriel Lima <36959874+gabriel-lima96@users.noreply.github.com>
2018-12-14 03:56:23 -0200

    Versão funcional
    versão funcional da atividade

---
+++ js/populate.js
@@ -0,0 +1,698 @@
+//Esse arquivo só tem o intuito de gerar a tabela para testes
+//Valores adquiridos em www.json-generator.com
+
+var populate = [
+    {
+      "nome": "Clayton Kelley",
+      "email": "claytonkelley@fortean.com",
+      "telefone": "(51) 99785-1728",
+      "nascimento": "13/08/2002",
+      "cidade": "Turpin"
+    },
+    {
+      "nome": "Ofelia Stevens",
+      "email": "ofeliastevens@fortean.com",
+      "telefone": "(51) 99124-5232",
+      "nascimento": "25/01/1982",
+      "cidade": "Navarre"
+    },
+    {
+      "nome": "Parker Lucas",
+      "email": "parkerlucas@fortean.com",
+      "telefone": "(51) 99054-7230",
+      "nascimento": "30/11/2002",
+      "cidade": "Cannondale"
+    },
+    {
+      "nome": "Dawn Morin",
+      "email": "dawnmorin@fortean.com",
+      "telefone": "(51) 99914-9531",
+      "nascimento": "01/11/2004",
+      "cidade": "Dixonville"
+    },
+    {
+      "nome": "Winifred Mcgowan",
+      "email": "winifredmcgowan@fortean.com",
+      "telefone": "(51) 99375-4629",
+      "nascimento": "27/10/2015",
+      "cidade": "Clinton"
+    },
+    {
+      "nome": "Gretchen Cohen",
+      "email": "gretchencohen@fortean.com",
+      "telefone": "(51) 98625-9439",
+      "nascimento": "14/04/1988",
+      "cidade": "Roulette"
+    },
+    {
+      "nome": "Merritt Rocha",
+      "email": "merrittrocha@fortean.com",
+      "telefone": "(51) 99844-0332",
+      "nascimento": "09/12/2012",
+      "cidade": "Kersey"
+    },
+    {
+      "nome": "Olivia Guy",
+      "email": "oliviaguy@fortean.com",
+      "telefone": "(51) 99455-6233",
+      "nascimento": "13/12/1994",
+      "cidade": "Falconaire"
+    },
+    {
+      "nome": "Celina Giles",
+      "email": "celinagiles@fortean.com",
+      "telefone": "(51) 98064-4832",
+      "nascimento": "25/06/1995",
+      "cidade": "Riviera"
+    },
+    {
+      "nome": "Eileen Phelps",
+      "email": "eileenphelps@fortean.com",
+      "telefone": "(51) 98624-1528",
+      "nascimento": "09/01/1992",
+      "cidade": "Rodanthe"
+    },
+    {
+      "nome": "Colette Reed",
+      "email": "colettereed@fortean.com",
+      "telefone": "(51) 99054-6232",
+      "nascimento": "23/09/2006",
+      "cidade": "Tuskahoma"
+    },
+    {
+      "nome": "Holman Key",
+      "email": "holmankey@fortean.com",
+      "telefone": "(51) 98785-9422",
+      "nascimento": "02/04/2015",
+      "cidade": "Hoagland"
+    },
+    {
+      "nome": "Kelley Sargent",
+      "email": "kelleysargent@fortean.com",
+      "telefone": "(51) 98285-5123",
+      "nascimento": "12/05/2013",
+      "cidade": "Albany"
+    },
+    {
+      "nome": "Todd Stein",
+      "email": "toddstein@fortean.com",
+      "telefone": "(51) 99685-6728",
+      "nascimento": "16/07/1991",
+      "cidade": "Alafaya"
+    },
+    {
+      "nome": "Ronda Osborn",
+      "email": "rondaosborn@fortean.com",
+      "telefone": "(51) 99565-5527",
+      "nascimento": "25/02/1983",
+      "cidade": "Deltaville"
+    },
+    {
+      "nome": "Robert Boyd",
+      "email": "robertboyd@fortean.com",
+      "telefone": "(51) 98104-8036",
+      "nascimento": "12/01/1994",
+      "cidade": "Galesville"
+    },
+    {
+      "nome": "Adrian Whitney",
+      "email": "adrianwhitney@fortean.com",
+      "telefone": "(51) 99085-0428",
+      "nascimento": "28/09/1996",
+      "cidade": "Rutherford"
+    },
+    {
+      "nome": "Erin Jacobson",
+      "email": "erinjacobson@fortean.com",
+      "telefone": "(51) 98725-6029",
+      "nascimento": "15/03/2003",
+      "cidade": "Kerby"
+    },
+    {
+      "nome": "Carlene Spence",
+      "email": "carlenespence@fortean.com",
+      "telefone": "(51) 98434-4132",
+      "nascimento": "27/11/1995",
+      "cidade": "Southmont"
+    },
+    {
+      "nome": "Beatriz Holt",
+      "email": "beatrizholt@fortean.com",
+      "telefone": "(51) 99864-0931",
+      "nascimento": "13/09/1988",
+      "cidade": "Tonopah"
+    },
+    {
+      "nome": "Alicia Gaines",
+      "email": "aliciagaines@fortean.com",
+      "telefone": "(51) 99764-7539",
+      "nascimento": "28/05/2012",
+      "cidade": "Fostoria"
+    },
+    {
+      "nome": "Hampton Andrews",
+      "email": "hamptonandrews@fortean.com",
+      "telefone": "(51) 99454-7422",
+      "nascimento": "06/07/1995",
+      "cidade": "Genoa"
+    },
+    {
+      "nome": "Greta Navarro",
+      "email": "gretanavarro@fortean.com",
+      "telefone": "(51) 98714-8638",
+      "nascimento": "04/03/1990",
+      "cidade": "Woodburn"
+    },
+    {
+      "nome": "Mckay Huff",
+      "email": "mckayhuff@fortean.com",
+      "telefone": "(51) 98015-4821",
+      "nascimento": "12/10/2002",
+      "cidade": "Blende"
+    },
+    {
+      "nome": "Haley Howard",
+      "email": "haleyhoward@fortean.com",
+      "telefone": "(51) 99474-8734",
+      "nascimento": "24/02/2017",
+      "cidade": "Indio"
+    },
+    {
+      "nome": "Henrietta Carter",
+      "email": "henriettacarter@fortean.com",
+      "telefone": "(51) 98754-9025",
+      "nascimento": "10/08/1990",
+      "cidade": "Chapin"
+    },
+    {
+      "nome": "Reyes Collier",
+      "email": "reyescollier@fortean.com",
+      "telefone": "(51) 98145-0024",
+      "nascimento": "05/02/1989",
+      "cidade": "Jacksonwald"
+    },
+    {
+      "nome": "Nettie Donovan",
+      "email": "nettiedonovan@fortean.com",
+      "telefone": "(51) 98185-1138",
+      "nascimento": "27/12/2011",
+      "cidade": "Topaz"
+    },
+    {
+      "nome": "Jo Parrish",
+      "email": "joparrish@fortean.com",
+      "telefone": "(51) 98045-3332",
+      "nascimento": "04/08/2004",
+      "cidade": "Fivepointville"
+    },
+    {
+      "nome": "Vazquez May",
+      "email": "vazquezmay@fortean.com",
+      "telefone": "(51) 99545-2439",
+      "nascimento": "26/07/1989",
+      "cidade": "Bagtown"
+    },
+    {
+      "nome": "Pace Mathis",
+      "email": "pacemathis@fortean.com",
+      "telefone": "(51) 98845-2038",
+      "nascimento": "29/04/1995",
+      "cidade": "Hollins"
+    },
+    {
+      "nome": "Chen Chan",
+      "email": "chenchan@fortean.com",
+      "telefone": "(51) 98194-7935",
+      "nascimento": "08/09/1983",
+      "cidade": "Kaka"
+    },
+    {
+      "nome": "Carey Walls",
+      "email": "careywalls@fortean.com",
+      "telefone": "(51) 98215-7628",
+      "nascimento": "25/08/2004",
+      "cidade": "Townsend"
+    },
+    {
+      "nome": "Crawford Harmon",
+      "email": "crawfordharmon@fortean.com",
+      "telefone": "(51) 99724-2334",
+      "nascimento": "10/10/1988",
+      "cidade": "Weogufka"
+    },
+    {
+      "nome": "Mcfarland Landry",
+      "email": "mcfarlandlandry@fortean.com",
+      "telefone": "(51) 98055-6839",
+      "nascimento": "04/02/1986",
+      "cidade": "Vivian"
+    },
+    {
+      "nome": "Stacie Beck",
+      "email": "staciebeck@fortean.com",
+      "telefone": "(51) 99864-3027",
+      "nascimento": "17/12/1993",
+      "cidade": "Yettem"
+    },
+    {
+      "nome": "Lilia Richard",
+      "email": "liliarichard@fortean.com",
+      "telefone": "(51) 98924-0030",
+      "nascimento": "15/04/1992",
+      "cidade": "Whitmer"
+    },
+    {
+      "nome": "Laverne Crawford",
+      "email": "lavernecrawford@fortean.com",
+      "telefone": "(51) 98195-1737",
+      "nascimento": "14/08/1983",
+      "cidade": "Camas"
+    },
+    {
+      "nome": "Mercer Compton",
+      "email": "mercercompton@fortean.com",
+      "telefone": "(51) 99655-4737",
+      "nascimento": "25/03/2018",
+      "cidade": "Retsof"
+    },
+    {
+      "nome": "Kimberly Winters",
+      "email": "kimberlywinters@fortean.com",
+      "telefone": "(51) 98125-5621",
+      "nascimento": "02/02/1995",
+      "cidade": "Mayfair"
+    },
+    {
+      "nome": "Amy Butler",
+      "email": "amybutler@fortean.com",
+      "telefone": "(51) 98414-6529",
+      "nascimento": "04/08/2010",
+      "cidade": "Brooktrails"
+    },
+    {
+      "nome": "Elma Vaughan",
+      "email": "elmavaughan@fortean.com",
+      "telefone": "(51) 98554-0926",
+      "nascimento": "18/04/2009",
+      "cidade": "Maybell"
+    },
+    {
+      "nome": "Amparo Reeves",
+      "email": "amparoreeves@fortean.com",
+      "telefone": "(51) 99964-4938",
+      "nascimento": "05/12/2008",
+      "cidade": "Groveville"
+    },
+    {
+      "nome": "Clare Rios",
+      "email": "clarerios@fortean.com",
+      "telefone": "(51) 98995-1826",
+      "nascimento": "28/04/2016",
+      "cidade": "Riegelwood"
+    },
+    {
+      "nome": "Eunice Barnett",
+      "email": "eunicebarnett@fortean.com",
+      "telefone": "(51) 98264-9639",
+      "nascimento": "01/05/1999",
+      "cidade": "Orason"
+    },
+    {
+      "nome": "Garcia Hogan",
+      "email": "garciahogan@fortean.com",
+      "telefone": "(51) 98694-5226",
+      "nascimento": "04/09/1992",
+      "cidade": "Cashtown"
+    },
+    {
+      "nome": "Vargas Gill",
+      "email": "vargasgill@fortean.com",
+      "telefone": "(51) 98665-8836",
+      "nascimento": "06/01/1985",
+      "cidade": "Interlochen"
+    },
+    {
+      "nome": "Anthony Pate",
+      "email": "anthonypate@fortean.com",
+      "telefone": "(51) 99604-3834",
+      "nascimento": "27/01/2016",
+      "cidade": "Wanship"
+    },
+    {
+      "nome": "Lewis Garcia",
+      "email": "lewisgarcia@fortean.com",
+      "telefone": "(51) 99234-5023",
+      "nascimento": "10/12/2001",
+      "cidade": "Martinsville"
+    },
+    {
+      "nome": "Gutierrez Townsend",
+      "email": "gutierreztownsend@fortean.com",
+      "telefone": "(51) 98975-0338",
+      "nascimento": "11/04/1989",
+      "cidade": "Marienthal"
+    },
+    {
+      "nome": "Janie Berger",
+      "email": "janieberger@fortean.com",
+      "telefone": "(51) 99644-0933",
+      "nascimento": "10/11/2010",
+      "cidade": "Gibsonia"
+    },
+    {
+      "nome": "Patton Valencia",
+      "email": "pattonvalencia@fortean.com",
+      "telefone": "(51) 99525-1730",
+      "nascimento": "21/01/2009",
+      "cidade": "Omar"
+    },
+    {
+      "nome": "Gibbs Frazier",
+      "email": "gibbsfrazier@fortean.com",
+      "telefone": "(51) 98134-9130",
+      "nascimento": "23/12/2006",
+      "cidade": "Lydia"
+    },
+    {
+      "nome": "Stewart Carson",
+      "email": "stewartcarson@fortean.com",
+      "telefone": "(51) 98094-7635",
+      "nascimento": "16/08/2017",
+      "cidade": "Brewster"
+    },
+    {
+      "nome": "Moss Good",
+      "email": "mossgood@fortean.com",
+      "telefone": "(51) 99235-6621",
+      "nascimento": "20/04/1996",
+      "cidade": "Jackpot"
+    },
+    {
+      "nome": "Marta Strickland",
+      "email": "martastrickland@fortean.com",
+      "telefone": "(51) 98235-4427",
+      "nascimento": "19/08/2008",
+      "cidade": "Shrewsbury"
+    },
+    {
+      "nome": "Patrice Vazquez",
+      "email": "patricevazquez@fortean.com",
+      "telefone": "(51) 99364-0933",
+      "nascimento": "26/04/1992",
+      "cidade": "Maplewood"
+    },
+    {
+      "nome": "Effie Hanson",
+      "email": "effiehanson@fortean.com",
+      "telefone": "(51) 98174-9827",
+      "nascimento": "20/02/2004",
+      "cidade": "Fairlee"
+    },
+    {
+      "nome": "Shana Stevenson",
+      "email": "shanastevenson@fortean.com",
+      "telefone": "(51) 99955-4638",
+      "nascimento": "05/10/1986",
+      "cidade": "Walland"
+    },
+    {
+      "nome": "Neal Hardin",
+      "email": "nealhardin@fortean.com",
+      "telefone": "(51) 99844-9634",
+      "nascimento": "27/11/2004",
+      "cidade": "Katonah"
+    },
+    {
+      "nome": "Sloan Bauer",
+      "email": "sloanbauer@fortean.com",
+      "telefone": "(51) 99644-0830",
+      "nascimento": "10/09/2001",
+      "cidade": "Cade"
+    },
+    {
+      "nome": "Bryant Hopkins",
+      "email": "bryanthopkins@fortean.com",
+      "telefone": "(51) 99095-0722",
+      "nascimento": "24/01/2014",
+      "cidade": "Hemlock"
+    },
+    {
+      "nome": "Ryan Cross",
+      "email": "ryancross@fortean.com",
+      "telefone": "(51) 98705-5026",
+      "nascimento": "17/11/1993",
+      "cidade": "Farmington"
+    },
+    {
+      "nome": "Perez Rosario",
+      "email": "perezrosario@fortean.com",
+      "telefone": "(51) 99365-9431",
+      "nascimento": "16/10/1995",
+      "cidade": "Snyderville"
+    },
+    {
+      "nome": "Elnora Lambert",
+      "email": "elnoralambert@fortean.com",
+      "telefone": "(51) 98515-3539",
+      "nascimento": "08/03/2007",
+      "cidade": "Venice"
+    },
+    {
+      "nome": "Sheryl Gilliam",
+      "email": "sherylgilliam@fortean.com",
+      "telefone": "(51) 98975-2326",
+      "nascimento": "01/06/2000",
+      "cidade": "Williamson"
+    },
+    {
+      "nome": "Hanson Blair",
+      "email": "hansonblair@fortean.com",
+      "telefone": "(51) 99195-6231",
+      "nascimento": "11/04/2008",
+      "cidade": "Kula"
+    },
+    {
+      "nome": "Ingrid Preston",
+      "email": "ingridpreston@fortean.com",
+      "telefone": "(51) 99075-1722",
+      "nascimento": "31/01/1999",
+      "cidade": "Titanic"
+    },
+    {
+      "nome": "Blackwell Owens",
+      "email": "blackwellowens@fortean.com",
+      "telefone": "(51) 98465-6620",
+      "nascimento": "13/09/1992",
+      "cidade": "Keller"
+    },
+    {
+      "nome": "Yang Travis",
+      "email": "yangtravis@fortean.com",
+      "telefone": "(51) 99725-3434",
+      "nascimento": "29/05/1980",
+      "cidade": "Jamestown"
+    },
+    {
+      "nome": "Poole Meyers",
+      "email": "poolemeyers@fortean.com",
+      "telefone": "(51) 99755-7226",
+      "nascimento": "20/12/1981",
+      "cidade": "Bowmansville"
+    },
+    {
+      "nome": "Whitley Patton",
+      "email": "whitleypatton@fortean.com",
+      "telefone": "(51) 99585-2921",
+      "nascimento": "18/06/2018",
+      "cidade": "Osmond"
+    },
+    {
+      "nome": "Bertha Garrison",
+      "email": "berthagarrison@fortean.com",
+      "telefone": "(51) 98225-6423",
+      "nascimento": "13/03/2014",
+      "cidade": "Winchester"
+    },
+    {
+      "nome": "Shelley Gregory",
+      "email": "shelleygregory@fortean.com",
+      "telefone": "(51) 99164-6421",
+      "nascimento": "28/07/1995",
+      "cidade": "Frystown"
+    },
+    {
+      "nome": "Rosalie Barker",
+      "email": "rosaliebarker@fortean.com",
+      "telefone": "(51) 99275-2832",
+      "nascimento": "09/01/2000",
+      "cidade": "Mahtowa"
+    },
+    {
+      "nome": "Kline Weiss",
+      "email": "klineweiss@fortean.com",
+      "telefone": "(51) 99874-3224",
+      "nascimento": "14/10/2015",
+      "cidade": "Comptche"
+    },
+    {
+      "nome": "Quinn Mcleod",
+      "email": "quinnmcleod@fortean.com",
+      "telefone": "(51) 98824-2722",
+      "nascimento": "03/08/1997",
+      "cidade": "Gardiner"
+    },
+    {
+      "nome": "Beulah Foster",
+      "email": "beulahfoster@fortean.com",
+      "telefone": "(51) 99845-5222",
+      "nascimento": "25/06/2009",
+      "cidade": "Edmund"
+    },
+    {
+      "nome": "Terrie Burt",
+      "email": "terrieburt@fortean.com",
+      "telefone": "(51) 98724-3428",
+      "nascimento": "07/08/1988",
+      "cidade": "Virgie"
+    },
+    {
+      "nome": "Gallagher Gould",
+      "email": "gallaghergould@fortean.com",
+      "telefone": "(51) 99675-8427",
+      "nascimento": "06/12/2017",
+      "cidade": "Reinerton"
+    },
+    {
+      "nome": "Horn Norris",
+      "email": "hornnorris@fortean.com",
+      "telefone": "(51) 98835-6938",
+      "nascimento": "16/04/2005",
+      "cidade": "Loveland"
+    },
+    {
+      "nome": "Spears Carr",
+      "email": "spearscarr@fortean.com",
+      "telefone": "(51) 99294-0934",
+      "nascimento": "09/05/1999",
+      "cidade": "Summertown"
+    },
+    {
+      "nome": "Chelsea Mckee",
+      "email": "chelseamckee@fortean.com",
+      "telefone": "(51) 99485-5826",
+      "nascimento": "27/01/2015",
+      "cidade": "Iola"
+    },
+    {
+      "nome": "Alisha Chang",
+      "email": "alishachang@fortean.com",
+      "telefone": "(51) 98215-8035",
+      "nascimento": "06/12/2005",
+      "cidade": "Durham"
+    },
+    {
+      "nome": "Norma Macias",
+      "email": "normamacias@fortean.com",
+      "telefone": "(51) 99954-5225",
+      "nascimento": "27/04/1985",
+      "cidade": "Orviston"
+    },
+    {
+      "nome": "Marva Holcomb",
+      "email": "marvaholcomb@fortean.com",
+      "telefone": "(51) 98734-1125",
+      "nascimento": "01/04/1988",
+      "cidade": "Basye"
+    },
+    {
+      "nome": "Candace Haynes",
+      "email": "candacehaynes@fortean.com",
+      "telefone": "(51) 98005-1428",
+      "nascimento": "01/05/2013",
+      "cidade": "Caroline"
+    },
+    {
+      "nome": "Head Schultz",
+      "email": "headschultz@fortean.com",
+      "telefone": "(51) 99364-3136",
+      "nascimento": "23/10/1987",
+      "cidade": "Callaghan"
+    },
+    {
+      "nome": "Rogers Mcbride",
+      "email": "rogersmcbride@fortean.com",
+      "telefone": "(51) 98894-1127",
+      "nascimento": "04/12/1988",
+      "cidade": "Linwood"
+    },
+    {
+      "nome": "Cooper Castro",
+      "email": "coopercastro@fortean.com",
+      "telefone": "(51) 98805-5622",
+      "nascimento": "31/05/2013",
+      "cidade": "Dahlen"
+    },
+    {
+      "nome": "Katina Kane",
+      "email": "katinakane@fortean.com",
+      "telefone": "(51) 99005-1527",
+      "nascimento": "29/07/1982",
+      "cidade": "Campo"
+    },
+    {
+      "nome": "Wanda Kent",
+      "email": "wandakent@fortean.com",
+      "telefone": "(51) 99125-6630",
+      "nascimento": "20/02/2013",
+      "cidade": "Darbydale"
+    },
+    {
+      "nome": "Lidia Sharp",
+      "email": "lidiasharp@fortean.com",
+      "telefone": "(51) 99505-8625",
+      "nascimento": "15/01/2018",
+      "cidade": "Leroy"
+    },
+    {
+      "nome": "Fern Bush",
+      "email": "fernbush@fortean.com",
+      "telefone": "(51) 98464-8321",
+      "nascimento": "01/08/1998",
+      "cidade": "Zarephath"
+    },
+    {
+      "nome": "Faith Vance",
+      "email": "faithvance@fortean.com",
+      "telefone": "(51) 98995-2426",
+      "nascimento": "02/11/2010",
+      "cidade": "Greenbush"
+    },
+    {
+      "nome": "Bridgett Byers",
+      "email": "bridgettbyers@fortean.com",
+      "telefone": "(51) 98735-1038",
+      "nascimento": "02/12/1987",
+      "cidade": "Norwood"
+    },
+    {
+      "nome": "Buckner Donaldson",
+      "email": "bucknerdonaldson@fortean.com",
+      "telefone": "(51) 98764-9624",
+      "nascimento": "17/10/1988",
+      "cidade": "Wright"
+    },
+    {
+      "nome": "Farley Hess",
+      "email": "farleyhess@fortean.com",
+      "telefone": "(51) 98665-4428",
+      "nascimento": "14/10/2001",
+      "cidade": "Watchtower"
+    },
+    {
+      "nome": "Cora Saunders",
+      "email": "corasaunders@fortean.com",
+      "telefone": "(51) 98734-2737",
+      "nascimento": "08/05/1990",
+      "cidade": "Idamay"
+    }
+  ]
